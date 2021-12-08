46a9a1f9f43a366715c9f288487ec6162320031e
Ioannis E. Kommas <johnkommas@gmail.com>
2021-12-08 13:29:09 +0200

    Ioannis E. Kommas

--- Build Chatbots with Python/Magic 8-Ball/app.py
+++ Build Chatbots with Python/Magic 8-Ball/app.py
@@ -1,58 +1,32 @@
-# TODO
-"""
-Setting up
-
-1.
-Declare a variable `name` and assign it to the name of the person who will be asking the Magic 8-Ball.
-
-2.
-Next, declare a variable `question`, and assign to a “Yes” or “No” question you’d like to ask the Magic 8-Ball.
-
-3.
-We want to store the answer of the Magic 8-Ball in another variable, which we’ll call `answer`.
-For now, assign this variable to an empty string.
-
-4.
-In order for the answer to be different each time we run the program, we will utilize randomly generated values.
-In Python, we can use the .randint() function from the random module to generate a random number from a range.
-
-5.
-Next, we’ll create a variable to store the randomly generated value.
-Declare a variable called random_number, and assign it to the function call:
-random.randint(1, 9)
-which will generate a random number between 1 (inclusive) and 9 (inclusive).
-Next, add a print() statement that outputs the value of random_number,
-and run the program several times to ensure random values are being generated as expected.
-Once you’re sure this is working as we expected, feel free to comment out this print() statement.
-
-6.
-Now that we’ve declared all the variables needed, it’s time to implement the core logic of our program!
-For this section, we’ll be utilizing control flow using an if/elif/else statement
-to assign different answers for each randomly generated value.
-First, write an if statement where if the random_number is equal to 1,
-answer is assigned to the phrase “Yes - definitely.”
-"""
 import random
 from answers import answer
-
+import time
 
 def run(__name):
+    step = [.2, .3]
     question = input("ΤΟ ΕΡΩΤΗΜΑ ΣΟΥ: ")
     if len(question) > 0:
         print(f"{__name} ΡΩΤΗΣΕ: {question}" if len(__name) > 0 else f'ΑΝΩΝΥΜΟ ΕΡΩΤΗΜΑ: {question}')
-        print(f'ΑΠΑΝΤΗΣΗ: {answer.get(random.randint(1, len(answer)), "ΣΦΑΛΜΑ")}')
+        print(f'ΑΠΑΝΤΗΣΗ: ', end='')
+        x = answer.get(random.randint(1, len(answer)), "ΣΦΑΛΜΑ")
+        for letter in x:
+            print(letter, end='')
+            time.sleep(random.choice(step))
+        print()
     else:
         return run(__name)
     return __name
 
 
-name = ''
-while True:
-    try:
-        if len(name) == 0:
-            name = input('ΤΟ ΟΝΟΜΑ ΣΟΥ: ')
-        name = run(name)
-    except KeyboardInterrupt:
-        print("\nGood By!!")
-        break
+def app():
+    name = ''
+    while True:
+        try:
+            if len(name) == 0:
+                name = input('ΤΟ ΟΝΟΜΑ ΣΟΥ: ')
+            name = run(name)
+        except KeyboardInterrupt:
+            print("\nGood By!!")
+            break
 
+app()
